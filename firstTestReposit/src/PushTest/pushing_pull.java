610291e4a8584f75b65dc56a68ab2ae45c6b5237
jayle <jayle@10.250.1.184>
2022-09-27 21:01:20 -0400

    Push test 21:01 - JL

---
+++ firstTestReposit/src/PushTest/pushing_pull.java
@@ -0,0 +1,11 @@
+package PushTest;
+
+public class pushing_pull {
+
+	public static void main(String[] args) {
+		int x = 1;
+		System.out.println(x);
+
+	}
+
+}
---
+++ firstTestReposit/src/firstTestReposit/Test_for_Group.java
@@ -0,0 +1,12 @@
+package firstTestReposit;
+
+public class Test_for_Group {
+
+	public static void main(String[] args) {
+		System.out.println("This is a test");
+		System.out.println(System.currentTimeMillis()/10000);
+		System.out.println("Time test");
+
+	}
+
+}
